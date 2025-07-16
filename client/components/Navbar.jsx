77e8bdee1ffd11376a87eadb942f0e3f8483ea6b
Pratik <patilpratikp035@gmail.com>
2025-07-16 18:41:41 +0530

    created navbar

---
+++ client/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="text-lg md:text-2xl font-bold px-4 py-3 md:px-15 md:py-7 border-b border-gray-300 shadow-lg bg-gray-100">
+        Rugas Technologies
+      </h1>
+    </div>
+  );
+};
+
+export default Navbar;
